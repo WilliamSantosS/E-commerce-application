929af11dc09fc76bf7274c8558742d721bc756b1
William santos <smurf455@hotmail.com>
2021-06-30 19:49:47 -0300

    creating the topbar and add product fields

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const routes = express.Router();
+const ProductController = require('./app/controllers/productController');
 
 routes.get('/', function (req, res) {
-    return res.send('conected')
+    return res.render('layout.njk')
 })
 
+routes.get('/products/create', ProductController.create) 
 
+routes.get("/ads/create", function(req, res) {
+    return res.redirect("/products/create")
+})
 
 module.exports =  routes; 
