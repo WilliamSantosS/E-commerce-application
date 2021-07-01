e79d4826cf4154c6544bc89df1c7b3683ca83bd1
William santos <smurf455@hotmail.com>
2021-07-01 18:48:39 -0300

    creating the product post method; form style; product and category model

--- src/app/controllers/productController.js
+++ src/app/controllers/productController.js
@@ -1,5 +1,34 @@
+const Category = require('../models/category');
+const Product = require('../models/product');
+
+
 module.exports = {
     create(req, res) {
-        return res.render("products/create.njk")
+
+        Category.all()
+            .then(function (results) {
+                const categories = results.rows;
+                return res.render("products/create.njk", { categories })
+            }).catch(function (err) {
+                throw new Error(err);
+            })
+    }, 
+
+    async post(req, res){
+        const keys = Object.keys(req.body)
+
+        for(key in keys) {
+            if(req.body[key] == "") {
+                return res.send("Please fill all the fields")
+            }
+        }
+
+        let result = await Product.create(req.body);
+        const productId = result.rows[0];
+
+        result = await Category.all()
+        const categories = result.rows[0]
+
+        return res.render('products/create.njk', { productId, categories })
     }
- }
+}
--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,8 @@ routes.get('/', function (req, res) {
     return res.render('layout.njk')
 })
 
-routes.get('/products/create', ProductController.create) 
+routes.get('/products/create', ProductController.create);
+routes.post('/products', ProductController.post)
 
 routes.get("/ads/create", function(req, res) {
     return res.redirect("/products/create")
