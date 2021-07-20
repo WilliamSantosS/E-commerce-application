b1dec5847b5aba30083c3f95095581d24cca2226
William santos <smurf455@hotmail.com>
2021-07-19 21:42:38 -0300

    Creating the products search methods

--- src/database.sql
+++ src/database.sql
@@ -1,29 +1,71 @@
 CREATE TABLE "products" (
   "id" SERIAL PRIMARY KEY,
   "category_id" int NOT NULL,
-  "user_id" int UNIQUE,
-  "name" text,
-  "description" text,
+  "user_id" int,
+  "name" text NOT NULL,
+  "description" text NOT NULL,
   "old_price" int,
-  "price" int,
-  "quantity" int,
-  "status" int,
+  "price" int NOT NULL,
+  "quantity" int DEFAULT 0,
+  "status" int DEFAULT 1,
   "created_at" timestamp DEFAULT (now()),
   "updated_at" timestamp DEFAULT (now())
 );
 
 CREATE TABLE "categories" (
   "id" SERIAL PRIMARY KEY,
-  "name" text
+  "name" text NOT NULL
 );
 
 CREATE TABLE "files" (
   "id" SERIAL PRIMARY KEY,
   "name" text,
-  "path" text,
+  "path" text NOT NULL,
   "products_id" int 
 );
 
 ALTER TABLE "products" ADD FOREIGN KEY ("category_id") REFERENCES "categories" ("id");
 
 ALTER TABLE "files" ADD FOREIGN KEY ("products_id") REFERENCES "products" ("id");
+
+
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "cpf_cnpj" int UNIQUE NOT NULL,
+  "cep" text,
+  "address" text,
+  "created_at" timestamp DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
+
+INSERT INTO categories(name) VALUES ('Food');
+INSERT INTO categories(name) VALUES ('Eletronic');
+INSERT INTO categories(name) VALUES ('Automotive');
+
+
+-- foreign key 
+ALTER TABLE "products" ADD FOREIGN KEY ('user_id') REFERENCES "users" ("id");
+
+-- create procedure
+CREATE FUNCTION trigger_set_timestamp()
+RETURN TRIGGER AS $$
+BEGIN
+ NEW.updated_at = NOW();
+ RETURN NEW; 
+END;
+$$ LANGUAGE plpgsql;
+
+-- auto updated_at products
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON products
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
+
+-- auto updated_at users
+CREATE TRIGGER set_timestamp
+BEFORE UPDATE ON users
+FOR EACH ROW
+EXECUTE PROCEDURE trigger_set_timestamp();
