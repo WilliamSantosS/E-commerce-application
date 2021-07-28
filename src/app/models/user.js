1d78c49406b812b8b7782c41815b285daf79c9f5
William santos <smurf455@hotmail.com>
2021-07-28 20:52:31 -0300

    using the base concept to deal with models

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -2,62 +2,45 @@ const db = require('../../config/db');
 const { hash } = require('bcryptjs');
 const Product = require('../models/product')
 const fs = require('fs')
+const Base = require('./base')
+Base.init({ table: 'users'})
 
 module.exports = {
-    async findOne(filters) {
-        let query = "SELECT * FROM users"
+    ...Base,
 
-        Object.keys(filters).map(key => {
+    // async create(data) {
 
-            // WHERE || OR
-            query = `
-                ${query} 
-                ${key}
-            `
+    //     try {
+    //         const query =
+    //             ` INSERT INTO users (
+    //             name,
+    //             email,
+    //             password,
+    //             cpf_cnpj,
+    //             cep,
+    //             address
+    //         ) VALUES ($1, $2, $3, $4, $5, $6)
+    //         RETURNING id
+    //          `
+    //         const passwordHash = await hash(data.password, 8)
 
-            Object.keys(filters[key]).map(field => {
-                query = `
-                    ${query} ${field} = '${filters[key][field]}'               
-                `
-            })
-        })
-        const results = await db.query(query)
-        return results.rows[0]
-    },
+    //         const values = [
+    //             data.name,
+    //             data.email,
+    //             passwordHash,
+    //             data.cpf_cnpj.replace(/\D/g, ""),
+    //             data.cep.replace(/\D/g, ""),
+    //             data.address
+    //         ]
 
-    async create(data) {
+    //         const results = await db.query(query, values)
+    //         return results.rows[0].id
 
-        try {
-            const query =
-                ` INSERT INTO users (
-                name,
-                email,
-                password,
-                cpf_cnpj,
-                cep,
-                address
-            ) VALUES ($1, $2, $3, $4, $5, $6)
-            RETURNING id
-             `
-            const passwordHash = await hash(data.password, 8)
+    //     } catch (err) {
+    //         console.error(err)
+    //     }
 
-            const values = [
-                data.name,
-                data.email,
-                passwordHash,
-                data.cpf_cnpj.replace(/\D/g, ""),
-                data.cep.replace(/\D/g, ""),
-                data.address
-            ]
-
-            const results = await db.query(query, values)
-            return results.rows[0].id
-
-        } catch (err) {
-            console.error(err)
-        }
-
-    },
+    // },
 
     async update(id, fields) {
         let query = "UPDATE users SET"
